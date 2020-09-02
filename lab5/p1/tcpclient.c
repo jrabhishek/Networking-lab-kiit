dfcc9e2e52a71303a31a7375df15de9cbb4f50b0
curatorRaj <1828043@kiit.ac.in>
2020-09-02 17:19:10 +0530

    lab 5 q1

---
+++ lab5/p1/tcpclient.c
@@ -0,0 +1,56 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    int sfd;
+
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1)
+    {
+        printf("Error in creating socket.\n");
+        exit(0);
+    }
+    struct sockaddr_in clientAddr, serverAddr;
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8083);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_port = htons(8087);
+    clientAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    int sbind;
+    sbind = bind(sfd, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
+    if (sbind == -1)
+    {
+        printf("Error in client binding.\n");
+        exit(0);
+    }
+    printf("Client bind successful.\n");
+
+    if (connect(sfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1)
+    {
+        printf("Error in connecting.\n");
+        exit(0);
+    }
+
+    int len = sizeof(clientAddr);
+
+    char message[100];
+    printf("Enter the message for server:");
+    scanf(" %[^\n]", message);
+    send(sfd, message, strlen(message), 0);
+    close(sfd);
+
+
+    return 0;
+}
---
+++ lab5/p1/tcpserver.c
@@ -0,0 +1,60 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+	int sfd;
+	
+	sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sfd==-1)
+	{
+		printf("Error in creating socket.\n");
+		exit(0);
+	}
+	struct sockaddr_in serverAddr;
+	
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(8083);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	int sbind;
+	sbind = bind(sfd , (struct sockaddr *) &serverAddr , sizeof(serverAddr));
+	if (sbind== -1)
+	{
+		printf("Error in server binding.\n");
+		exit(0);		
+	} 
+	printf("Server bind successful.\n");
+	
+	if(listen(sfd, 5))
+	{
+		printf("Listen limit reached.\n");
+		exit(0);
+	}
+	
+	char buffer[100];
+	struct sockaddr_in clientAddr;
+	int len = sizeof(clientAddr);
+	
+	int nfd = accept(sfd, (struct sockaddr*)&clientAddr, &len);
+	if(nfd==-1)
+	{
+		printf("Error in accepting.\n");
+		exit(0);
+	}
+	
+	int recvLen = recv(nfd, buffer, 100, 0);
+	
+	buffer[recvLen]='\0';
+	printf("%s\n", buffer);
+	close(nfd);
+	return 0;
+}
+	
